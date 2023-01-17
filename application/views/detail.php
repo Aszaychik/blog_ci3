3915c441fadde7f3fb2b99434bef0ecb98594579
AsZaychik <aszaychik@gmail.com>
2023-01-18 01:05:26 +0700

    Image fix

--- application/views/detail.php
+++ application/views/detail.php
@@ -4,7 +4,7 @@ $this->load->view('partials/header');
 <!-- Page Header-->
 <?php 
 if(empty($blog['cover'])){
-    $cover = 'https://scontent.fsub8-2.fna.fbcdn.net/v/t39.30808-6/310195025_1496125947476402_6729970876596988792_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeGxRrhZhJa7_lo2o-qlrtGJOU9dMVOwtsA5T10xU7C2wMrTmOY80c9rKPKBweDDZ2LVSVEYoOeBKPkb0e1s_4W6&_nc_ohc=O6Vw0h5xrJEAX-ac0a0&tn=og2zDuZcDLaZyeoO&_nc_ht=scontent.fsub8-2.fna&oh=00_AfBRNj-sUTzyo8kz1vx_e0_Dv6Trj6GwBx6c_VSaaIyz3A&oe=637877DD';
+    $cover = 'https://source.unsplash.com/random';
 
 }else{
     $cover = base_url() . 'uploads/' . $blog['cover'];
